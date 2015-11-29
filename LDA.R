96e174d60c791ca72349453521dcd9ab40d5d32d
BT <brtsay@ucsd.edu>
2015-11-28 20:43:37 -0800

    Fixes bag of words

--- LDA.R
+++ LDA.R
@@ -46,51 +46,53 @@ uid.list <- sort(unique(c(train$uid, train$retweeted_uid)), decreasing=TRUE)
 write.table(uid.list, "~/Documents/CSE190_Data/uid_list.csv", row.names = FALSE, col.names = "uid")
 
 #####################################################################
-setwd("/mainstorage/briant/mpsaweibo/CSE190")
+## setwd("/mainstorage/briant/mpsaweibo/CSE190")
+setwd("/home/b/Documents/CSE190_Data")
 
-## stopwords <- read.table("/media/b/DEF8DBF5F8DBC9C3/Users/B T/Copy/CSE190/Assignment/CSE190_Assignment2/stopwords.txt")
-stopwords <- read.table("stopwords.txt")
+stopwords <- read.table("/media/b/DEF8DBF5F8DBC9C3/Users/B T/Copy/CSE190/Assignment/CSE190_Assignment2/stopwords.txt")
+## stopwords <- read.table("stopwords.txt")
 stopwords <- enc2utf8(as.character(stopwords$V1))
 
 
-## clean
+## clean (run on server)
+## text <- c(train$text, valid$text, test$text)
+## text <- gsub("u.*", "", text)
+## text.source <- Corpus(VectorSource(text))
+## text.dtm <- DocumentTermMatrix(text.source,
+##                                control = list(removePunctuation = T,
+##                                               removeNumbers = T,
+##                                               stopwords = stopwords,
+##                                               wordLengths = c(2, Inf),
+##                                               weighting = function (x) weightTfIdf(x)
+##                                               ))
+## load in text dtm
 train <- fread("pre_train.csv")
-train.text <- train$text
-## remove everything that starts with a u
-train.text <- gsub("u.*", "", train.text)
-train.text.source <- Corpus(VectorSource(train.text))
-train.text.dtm <- DocumentTermMatrix(train.text.source,
-                               control = list(removePunctuation = T,
-                                              removeNumbers = T,
-                                              stopwords = stopwords,
-                                              wordLengths = c(2, Inf),
-                                              weighting = function (x) weightTfIdf(x)
-                                              ))
-train.terms <- Terms(train.text.dtm)
+valid <- fread("pre_valid.csv")
+test <- fread("pre_test.csv")
+attach("text_dtm.RData")
+train.text.dtm <- text.dtm[1:nrow(train),]
+valid.text.dtm <- text.dtm[(nrow(train)+1):(nrow(train)+nrow(valid)),]
+test.text.dtm <- text.dtm[(nrow(train)+nrow(valid)+1):nrow(text.dtm),]
 
 train.feat <- fread("train_feat.csv")
-train.feat <- as.simple_triplet_matrix(train.feat)
-## combine features and bag of words
-train.X <- cbind(train.text.dtm, train.feat)
-## convert to format that liblinear can use
-train.X <- sparseMatrix(train.X$i, train.X$j, x=train.X$v)
-train.X <- as(train.X, "matrix.csr")
-train.y <- train$permission_denied
-train.y[is.na(train.y)] <- 0
+valid.feat <- fread("valid_feat.csv")
+test.feat <- fread("test_feat.csv")
 
-valid <- fread("pre_valid.csv")
-valid.text <- valid$text
-## remove everything that starts with a u
-valid.text <- gsub("u.*", "", valid.text)
-valid.text.source <- Corpus(VectorSource(valid.text))
-valid.text.dtm <- DocumentTermMatrix(valid.text.source,
-                               control = list(removePunctuation = T,
-                                              removeNumbers = T,
-                                              stopwords = stopwords,
-                                              wordLengths = c(2, Inf),
-                                              dictionary = train.terms,
-                                              weighting = function (x) weightTfIdf(x)
-                                              ))
+mergeFeatures <- function(set, feat, text.dtm) {
+    feat <- as.simple_triplet_matrix(train.feat)
+    ## combine features and bag of words
+    X <- cbind(text.dtm, feat)
+    ## convert to format that liblinear can use
+    X <- sparseMatrix(X$i, X$j, x=X$v)
+    X <- as(X, "matrix.csr")
+    y <- set$permission_denied
+    y[is.na(y)] <- 0
+    return(list(X,y))
+}
+
+train.data <- mergeFeatures(train, train.feat, train.text.dtm)
+valid.data <- mergeFeatures(valid, valid.feat, valid.text.dtm)
+
 
 findError <- function(pred, true) {sum(abs(pred$predictions - true)/length(true))}
 
@@ -102,6 +104,8 @@ model <- LiblineaR(train.X, train.y)
 
 ## wordcloud
 stopwords.temp <- c(stopwords, "link", "the", "via")
+cens <- fread("cens_tweets_seg.csv")
+text.source <- Corpus(VectorSource(cens))
 text.source <- tm_map(text.source, removePunctuation)
 text.source <- tm_map(text.source, removeNumbers)
 text.source <- tm_map(text.source, removeWords, stopwords.temp)
@@ -123,3 +127,32 @@ text.dtm <- DocumentTermMatrix(text.source.nempty,
 text.ctm <- CTM(text.dtm, k=10)
 Topics <- topics(text.ctm)
 Terms <- terms(text.ctm, 10)
+
+#####################################################################
+## might need later?
+## train.text <- train$text
+## remove everything that starts with a u
+## train.text <- gsub("u.*", "", train.text)
+## train.text.source <- Corpus(VectorSource(train.text))
+## train.text.dtm <- DocumentTermMatrix(train.text.source,
+##                                control = list(removePunctuation = T,
+##                                               removeNumbers = T,
+##                                               stopwords = stopwords,
+##                                               wordLengths = c(2, Inf),
+##                                               weighting = function (x) weightTfIdf(x)
+##                                               ))
+## train.terms <- Terms(train.text.dtm)
+
+## valid <- fread("pre_valid.csv")
+## valid.text <- valid$text
+## ## remove everything that starts with a u
+## valid.text <- gsub("u.*", "", valid.text)
+## valid.text.source <- Corpus(VectorSource(valid.text))
+## valid.text.dtm <- DocumentTermMatrix(valid.text.source,
+##                                control = list(removePunctuation = T,
+##                                               removeNumbers = T,
+##                                               stopwords = stopwords,
+##                                               wordLengths = c(2, Inf),
+##                                               dictionary = train.terms,
+##                                               weighting = function (x) weightTfIdf(x)
+##                                               ))
